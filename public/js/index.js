95938bfb1ef6b03e654687f5d7f24ce2a80375af
Timi Ombe <timmyombe@gmail.com>
2022-08-27 17:09:08 +0100

    tracking values and screen output implemented

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,69 @@
-// Get elements
-const zero = document.getElementsByClassName("zero");
-const one = document.getElementsByClassName("one");
-const two = document.getElementsByClassName("two");
-const three = document.getElementsByClassName("three");
-const four = document.getElementsByClassName("four");
-const five = document.getElementsByClassName("five");
-const six = document.getElementsByClassName("six");
-const seven = document.getElementsByClassName("seven");
-const eight = document.getElementsByClassName("eight");
-const nine = document.getElementsByClassName("nine");
-const equalBtn = document.getElementsByClassName("equals");
-const addBtn = document.getElementsByClassName("add");
-const subtractBtn= document.getElementsByClassName("subtract");
-const multiplyBtn = document.getElementsByClassName("multiply");
-const divideBtn = document.getElementsByClassName("divide-by");
-const clearBtn = document.getElementsByClassName("clear");
-const deleteBtn = document.getElementsByClassName("delete");
-const dotBtn = document.getElementsByClassName("dot");
+// Get screen 
+const display = document.getElementById("display");
+
+let output = "";
+let pendingNumbers = [0];
+
+// Numeric operations
+document.querySelectorAll(".numbers").forEach(item => {
+    item.addEventListener("click", () => addToStack(item, true));
+});
+
+// Non-numeric Operations 
+document.querySelectorAll(".operations").forEach(item => {
+    item.addEventListener("click", () => addToStack(item, false));
+});
+
+
+
+// Helper functions
+function addToStack(btn, isNumeric) {
+    // Adds numerical values to the screen and keeps track
+    // of then via pendingNumbers.
+    let textValue = btn.textContent;
+    
+    if (isNumeric) {
+        output += textValue;
+        let end = pendingNumbers.length < 1 ? 0 : pendingNumbers.length - 1
+        if (output[output.length - 2] == ".") {
+            pendingNumbers[end] = parseFloat(pendingNumbers[end].toString() + `.${textValue}`); 
+        } else {
+            pendingNumbers[end] = parseFloat(pendingNumbers[end].toString() + textValue); 
+        } 
+        console.log(output, pendingNumbers);
+    } else {
+        // output += texvalue
+
+        if (textValue === " = ") {
+
+        } else if (textValue === " + ") {
+            output += textValue;
+            pendingNumbers.push(0);
+
+        } else if (textValue === " - ") {
+            output += textValue;
+            pendingNumbers.push(0);
+
+        } else if (textValue === " * ") {
+            output += textValue;
+            pendingNumbers.push(0);
+
+        } else if (textValue === " / ") {
+            output += textValue;
+            pendingNumbers.push(0);
+
+        } else if (textValue === " clear ") {
+            output = "";
+            pendingNumbers = [0];
+
+        } else if (textValue === " delete ") {
+            let strEnd = output.split(" ").reverse().join(" ").indexOf(" ")
+            output = output.slice(0, strEnd);
+            pendingNumbers.pop()
+
+        } else if (textValue === " . ") {
+            output += "."
+        }
+        console.log(output, pendingNumbers); 
+    }
+}
